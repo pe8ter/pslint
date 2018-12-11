e9d9fd9d619a4e23624154fd52db77195a2d4659
Peter Safranek <pe8ter@mac.com>
2018-12-11 00:27:32 -0800

    Minor cleanup
    * Remove duplicates from tsconfig.json.
    * Switch to JSDoc-style comments for license and classes.

--- src/noCatchExpectRule.ts
+++ src/noCatchExpectRule.ts
@@ -1,31 +1,34 @@
-/*
-    Copyright 2018 Peter Safranek
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
+/**
+ * @license
+ * Copyright 2018 Peter Safranek
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import * as ts from 'typescript';
-import * as TSlint from 'tslint';
+import * as TSLint from 'tslint';
 
-// This rule makes sure that Jasmine's `expect()` and `expectAsync()` functions aren't
-// called within a `catch` block, which may cause tests to incorrectly succeed.
-//
-// Parts copied from the TSLint project:
-// https://github.com/palantir/tslint/blob/7c841c0/src/rules/banRule.ts
+/**
+ * This rule makes sure that Jasmine's `expect()` and `expectAsync()` functions aren't
+ * called within a `catch` block, which may cause tests to incorrectly succeed.
+ *
+ * Parts copied from the TSLint project:
+ * https://github.com/palantir/tslint/blob/7c841c0/src/rules/banRule.ts
+ */
 
-export class Rule extends TSlint.Rules.AbstractRule {
+ export class Rule extends TSLint.Rules.AbstractRule {
 
-    public apply(sourceFile: ts.SourceFile): TSlint.RuleFailure[] {
+    public apply(sourceFile: ts.SourceFile): TSLint.RuleFailure[] {
         return this.applyWithWalker(new NoCatchExpectWalker(sourceFile, this.getOptions()));
     }
 }
@@ -33,7 +36,7 @@ export class Rule extends TSlint.Rules.AbstractRule {
 const EXPECT = 'expect';
 const EXPECT_ASYNC = 'expectAsync';
 
-class NoCatchExpectWalker extends TSlint.RuleWalker {
+class NoCatchExpectWalker extends TSLint.RuleWalker {
 
     public visitCallExpression(node: ts.CallExpression): void {
 
