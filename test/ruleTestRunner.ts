2ebc50ff23ff9c0007587bc775fb9f47a32ca179
Peter Safranek <pe8ter@mac.com>
2018-12-17 19:55:44 -0800

    Add test runner

---
+++ test/ruleTestRunner.ts
@@ -0,0 +1,44 @@
+/**
+ * @license
+ * Copyright 2018 Peter Safranek
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Test } from 'tslint';
+import * as glob from 'glob';
+import chalk from 'chalk';
+import { dirname } from 'path';
+
+// Test runner copied from the TSLint project.
+// Credit: https://github.com/palantir/tslint/commit/6b0af8fabb4bc98338d8912a693f5f851812dfff
+
+const RULES_DIRECTORY = 'src';
+
+process.stdout.write(chalk.underline('\nTesting lint rules:\n'));
+
+const testDirectories = glob.sync('test/rules/**/tslint.json').map(dirname);
+
+for (const testDirectory of testDirectories) {
+
+    const results = Test.runTest(testDirectory, RULES_DIRECTORY);
+    const didAllTestsPass = Test.consoleTestResultHandler(results, {
+        log(m) { process.stdout.write(m) },
+        error(m) { process.stderr.write(m) },
+    });
+
+    if (!didAllTestsPass) {
+        process.exitCode = 1;
+        break;
+    }
+}
